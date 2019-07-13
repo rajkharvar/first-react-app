c4f09d1577ceb3ca4f3e535837789a7500038b8d
rpk79 <rajkharvar1808@gmail.com>
2019-07-13 18:21:30 +0530

    fixed context state bug

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,27 +3,29 @@ import { connect } from 'react-redux';
 import { ThemeContext } from '../context/ThemeContext';
 
 class Counter extends Component {
-  static context = ThemeContext;
+  static contextType = ThemeContext;
   render() {
-    // const { isDarkTheme, light, dark } = this.context;
-    console.log(this.context);
-    // const theme = isDarkTheme ? dark : light;
+    const { isDarkTheme, light, dark, toggleTheme } = this.context;
+    const theme = isDarkTheme ? dark : light;
     return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-around'
-          // backgroundColor: theme.bg,
-          // color: theme.text
-        }}
-      >
-        <button type='button' onClick={() => this.props.inc()}>
-          +
-        </button>
-        <h1>{this.props.counter}</h1>
-        <button type='button' onClick={() => this.props.dec()}>
-          -
-        </button>
+      <div>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-around',
+            backgroundColor: theme.bg,
+            color: theme.text
+          }}
+        >
+          <button type='button' onClick={() => this.props.inc()}>
+            +
+          </button>
+          <h1>{this.props.counter}</h1>
+          <button type='button' onClick={() => this.props.dec()}>
+            -
+          </button>
+        </div>
       </div>
     );
   }
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -7,9 +7,14 @@ class ThemeContextProvider extends Component {
     dark: { bg: '#333', text: '#ccc' },
     light: { bg: '#ccc', text: '#333' }
   };
+  toggleTheme = () => {
+    this.setState({ isDarkTheme: !this.state.isDarkTheme });
+  };
   render() {
     return (
-      <ThemeContext.Provider value={{ ...this.state }}>
+      <ThemeContext.Provider
+        value={{ ...this.state, toggleTheme: this.toggleTheme }}
+      >
         {this.props.children}
       </ThemeContext.Provider>
     );
